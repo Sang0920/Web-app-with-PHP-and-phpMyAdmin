710f365435c686465ae3b80096718591325c81a2
Đỗ Thế Sang <79999787+Sang0920@users.noreply.github.com>
2023-05-29 04:36:36 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$DB_USER = 'shoeshop_admin';
+$DB_USER_PWD = 'YOUR_PWD';
+$DB_HOST = 'YOUR_HOST';
+$DB_NAME = 'shoeshopdb';
+$DB_DSN = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8";
+
+$one_week = time() + (7 * 24 * 60 * 60);
+
+$EMAIL_PWD = 'YOUR_SMTP_EMAIL_PWD';
